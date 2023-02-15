cb762c595b45efb3f708f4f412214fe1bbac7cdc
ikuTenss <125500639+ikuTenss@users.noreply.github.com>
2023-02-15 21:39:56 +0200

    Update r.lua

--- r.lua
+++ r.lua
@@ -1,12 +1,16 @@
-repeat wait() until game.Loaded
+repeat
+    wait()
+until game.Loaded
 game:GetService("RunService"):Set3dRenderingEnabled(false)
---setfpscap(10)
+setfpscap(30)
 task.wait(5)
-
 if game.PlaceId == 5411459567 then
-    repeat task.wait(1) until game.Workspace.Message
+    repeat
+        task.wait(1)
+    until game.Workspace.Message
 end
 _G.MinTimer = 60
+
 local function notify(text)
     game.StarterGui:SetCore("SendNotification", {
         Title = "RZiln's AFK hop",
@@ -15,13 +19,25 @@ local function notify(text)
     })
 end
 
+local function serverHop()
+    if syn then
+        syn.queue_on_teleport(
+            'loadstring(game:HttpGet("https://raw.githubusercontent.com/RZiln/GayBloxScripts/master/ABA/main.lua", true))()')
+    else
+        queue_on_teleport(
+            'loadstring(game:HttpGet("https://raw.githubusercontent.com/RZiln/GayBloxScripts/master/ABA/main.lua", true))()')
+    end
+
+end
+
 if game.GameId == 578392296 then
     if game.PlaceId ~= 5411459567 then
-        queue_on_teleport(
-            'loadstring(game:HttpGet("https://raw.githubusercontent.com/ikuTenss/the-zaza/main/r", true))()')
+        notify("Joining AFK World, please")
+        serverHop()
         game:GetService('TeleportService'):Teleport(5411459567)
     end
 
+    notify("AFK Hop running")
     local function timerCheck()
         local timer = game.Workspace.Message.Text:split(" ")
         local time = tonumber(timer[1])
@@ -29,16 +45,17 @@ if game.GameId == 578392296 then
     end
 
     if timerCheck() <= _G.MinTimer then
+        notify("Waiting for reward")
         repeat
             task.wait()
         until timerCheck() > _G.MinTimer
+        notify("Server Hopping")
         task.wait(1)
-        queue_on_teleport(
-            'loadstring(game:HttpGet("https://raw.githubusercontent.com/ikuTenss/the-zaza/main/r", true))()')
+        serverHop()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/RZiln/GayBloxScripts/master/ABA/ServerHop.lua", true))()
     else
-        queue_on_teleport(
-            'loadstring(game:HttpGet("https://raw.githubusercontent.com/ikuTenss/the-zaza/main/r", true))()')
+        notify("Server Hopping")
+        serverHop()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/RZiln/GayBloxScripts/master/ABA/ServerHop.lua", true))()
     end
 
